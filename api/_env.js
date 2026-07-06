31afafd6655e51194e99b892e79a4fbf0de1ac12
Laura Izarra <lauraizarra@Laura-Izarra-CO.local>
2026-07-06 16:00:44 -0500

    Clean local env loading for production

--- api/_env.js
+++ api/_env.js
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.local' });
-dotenv.config({ path: '.env' });
+if (!process.env.VERCEL) {
+  dotenv.config({ path: '.env.local' });
+  dotenv.config({ path: '.env' });
+}
